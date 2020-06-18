bc1bc739b02313942b74f5a5a64c6b276967b09c
darkshark400 <51637671+darkshark400@users.noreply.github.com>
2020-06-18 19:32:44 +0200

    ff

--- site/desktop/view/connectionView.php
+++ site/desktop/view/connectionView.php
@@ -3,20 +3,28 @@
   <head>
     <meta charset="utf-8">
     <title>Connexion</title>
-    <link rel="stylesheet" href="../config/stylesheet_desktop/theme_position.css">
+    <link rel="stylesheet" href="../config/stylesheet_desktop/theme_login.css">
   </head>
 
 
   <body>
-    <div id="">
-      <form class="#" action="../model/connectionModel.php" method="post">
-        <?php require_once('../model/auth-google.php'); ?>
+    <div id="container_body">
+      <form id=connexion_box action="../model/connectionModel.php" method="post">
+        <div id=logo>
+          <img class=image_logo src="../../../icons/blog.png" alt="logo blog">
+        </div>
+
         <!-- auth with google -->
-        <input type="text" name="username" placeholder="Nom d'utilisateur">
-        <input type="password" name="password" placeholder="Mot de passe">
-        <input type="submit" name="submit" value="Se connecter">
+        <div id=login_info_container>
+          <p class=texte_login> Connectez-vous avec : <p>
+          <div class=login_google><?php require_once('../model/auth-google.php'); ?></div>
+          <p class=texte_login> Ou : </p>
+          <input class=login_info type="text" name="username" placeholder="Nom d'utilisateur / Email">
+          <input class=login_info type="password" name="password" placeholder="Mot de passe">
+          <a class=texte_login_2 href="#">Mot de passe oublié ?</a>
+          <input class=login_button type="submit" name="submit" value="Se connecter">
+        </div>
       </form>
-      <a href="#">Mot de passe / nom d'utilisateur oublié ?</a>
     </div>
 
 
