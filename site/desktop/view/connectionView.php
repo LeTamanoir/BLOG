09732a38da682827adbd9e9b514f9d0f75999e4a
darkshark400 <51637671+darkshark400@users.noreply.github.com>
2020-06-18 20:15:44 +0200

    d

--- site/desktop/view/connectionView.php
+++ site/desktop/view/connectionView.php
@@ -7,33 +7,25 @@
   </head>
 
 
-  <body>
-    <div id="container_body">
-      <form id=connexion_box action="../model/connectionModel.php" method="post">
-        <div id=logo>
-          <img class=image_logo src="../../../icons/blog.png" alt="logo blog">
-        </div>
-
+  <body id="container_body">
+    <form id=connexion_box action="../model/connectionModel.php" method="post">
+      <div id=logo>
+        <img class=image_logo src="../../../icons/blog.png" alt="logo blog">
+      </div>
         <!-- auth with google -->
-        <div id=login_info_container>
-          <p class=texte_login> Connectez-vous avec : <p>
-          <div class=login_google><?php require_once('../model/auth-google.php'); ?></div>
-          <p class=texte_login> Ou : </p>
-          <input class=login_info type="text" name="username" placeholder="Nom d'utilisateur / Email">
-          <input class=login_info type="password" name="password" placeholder="Mot de passe">
-          <a class=texte_login_2 href="#">Mot de passe oublié ?</a>
-          <input class=login_button type="submit" name="submit" value="Se connecter">
-        </div>
-      </form>
-    </div>
-
-
+      <div id=login_info_container>
+        <p class=texte_login> Connectez-vous avec : <p>
+        <div class=login_google><?php require_once('../model/auth-google.php'); ?></div>
+        <p class=texte_login> Ou : </p>
+        <input class=login_info type="text" name="username" placeholder="Nom d'utilisateur / Email">
+        <input class=login_info type="password" name="password" placeholder="Mot de passe">
+        <a class=texte_login_2 href="#">Mot de passe oublié ?</a>
+        <input class=login_button type="submit" name="submit" value="Se connecter">
+      </div>
+    </form>
 
   </body>
-
   <footer>
     <?php   require_once('footer.html'); ?>
   </footer>
-
-
 </html>
