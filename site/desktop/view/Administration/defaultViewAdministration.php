16d5d55398b4102aaad052e8a53bca29fc56eee9
darkshark400 <51637671+darkshark400@users.noreply.github.com>
2020-06-20 00:05:31 +0200

    update

--- site/desktop/view/Administration/defaultViewAdministration.php
+++ site/desktop/view/Administration/defaultViewAdministration.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Accueil</title>
-    <link rel="stylesheet" href="../config/stylesheet_desktop/theme_main.css">
+    <link rel="stylesheet" href="../../config/stylesheet_desktop/theme_main.css">
   </head>
 
   <header><!-- --></header>
@@ -12,17 +12,19 @@
   <body id=container_body>
 
     <div>
-      <nav id=navbar>
+    <button id=capteur_navbar><img class=image_navbar src="../../../../icons/navbar.png" alt="navbar wheel">
+      <nav id=container_navbar>
         <ul>
-          <li><a href="#">Tableau de bord</a></li>
-          <li><a href="#">test</a></li>
-          <li><a href="#">Base de données</a></li>
-          <li><a href="#">Liste des utilisateurs</a></li>
-          <li><a href="#">Paramètres</a></li>
-          <li><a href="#">Déconnexion</a></li>
+          <li class=navbar_content><a href="#">Tableau de bord</a></li>
+          <li class=navbar_content><a href="#">test</a></li>
+          <li class=navbar_content><a href="#">Base de données</a></li>
+          <li class=navbar_content><a href="#">Liste des utilisateurs</a></li>
+          <li class=navbar_content><a href="#">Paramètres</a></li>
+          <li class=navbar_content><a href="#">Déconnexion</a></li>
         </ul>
     <!-- navigation bar -->
       </nav>
+    </button>
     </div>
 
     <div>
--- site/desktop/view/Public/defaultViewPublic.php
+++ site/desktop/view/Public/defaultViewPublic.php
@@ -3,24 +3,26 @@
   <head>
     <meta charset="utf-8">
     <title>Accueil</title>
-    <link rel="stylesheet" href="../config/stylesheet_desktop/theme_main.css">
+    <link rel="stylesheet" href="../../config/stylesheet_desktop/theme_main.css">
     <!--<link rel="stylesheet" href="../config/stylesheet_desktop/theme_position.css">-->
   </head>
 
-  <body id=body>
+  <body id=container_body>
   <!-- top of page -->
   <div>
-    <nav id=navbar>
-      <ul>
-        <li><a href="#">Liste des pauses lectures à corriger</a></li>
-        <li><a href="#">Pause/lecture publiques</a></li>
-        <li><a href="#">Pause/lecture non publiques</a></li>
-        <li><a href="#">Publier un message (pour les autres élèves)</a></li>
-      </ul>
-    </nav>
+    <button id=capteur_navbar><img class=image_navbar src="../../../../icons/navbar.png" alt="navbar wheel">
+      <nav id=container_navbar>
+        <ul>
+          <li class=navbar_content><a href="#">Liste des pauses lectures à corriger</a></li>
+          <li class=navbar_content><a href="#">Pause/lecture publiques</a></li>
+          <li class=navbar_content><a href="#">Pause/lecture non publiques</a></li>
+          <li class=navbar_content><a href="#">Publier un message (pour les autres élèves)</a></li>
+        </ul>
+      </nav>
+    </button>
   </div>
 
-  <div><a href="#">Mon profil</a></div>
+  <div id=profil><a href="#">Mon profil</a></div>
 
   <div id=container_footer><?php require_once('footer.html'); ?></div>
 
