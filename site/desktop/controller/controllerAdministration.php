39510196870c9b5b5eb0cbac60b26d9041f8ca25
Shuk029920 <arthurtesx@gmail.com>
2020-06-19 11:36:06 +0200

    update

--- site/desktop/controller/controllerAdministration.php
+++ site/desktop/controller/controllerAdministration.php
@@ -1,7 +1,7 @@
 <?php
-
+require_once('../config/dbConnect.php');
 require_once('../model/backend/modelAdministration.php');
-require_once('../view/Administration/defaultViewAdministration.html')
+require_once('../view/Administration/defaultViewAdministration.php');
 
 
 ?>
---
+++ site/desktop/view/Administration/defaultViewAdministration.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Accueil</title>
+    <!--<link rel="stylesheet" href="../../config/stylesheet_desktop/theme_login.css">-->
+  </head>
+
+  <header><!-- --></header>
+
+  <body>
+
+    <nav id=navbar>
+
+      <div class='navbar_element capteur1'>
+        <div class=test>theme</div>
+        <div id=theme_container>
+          <div class=theme_sombre>sombre</div>
+          <div class=theme_wood>bois</div>
+          <div class=theme_rainbow>arc-en-ciel</div>
+        </div>
+      </div>
+
+      <div class='navbar_element capteur2'>
+        <div class=test>test1</div>
+      </div>
+
+      <div class='navbar_element capteur3'>
+        <div class=test>test2</div>
+      </div>
+
+      <div class='navbar_element capteur4'>
+        <div class=test>test3</div>
+      </div>
+
+      <div class='navbar_element capteur5'>
+        <div class=test>test4</div>
+      </div>
+
+    <!-- navigation bar -->
+    </nav>
+
+
+  </body>
+
+  <footer><?php require_once('footer.html') ?></footer>
+</html>
