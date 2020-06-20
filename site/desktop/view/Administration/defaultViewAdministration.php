16528cd0d32e68cea29684628931ae099c40f2c6
darkshark400 <51637671+darkshark400@users.noreply.github.com>
2020-06-21 00:38:48 +0200

    update

--- site/desktop/view/Administration/defaultViewAdministration.php
+++ site/desktop/view/Administration/defaultViewAdministration.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <title>Accueil</title>
     <link rel="stylesheet" href="../../config/stylesheet_desktop/theme_main.css">
+    <link rel="stylesheet" href="../../config/stylesheet_desktop/theme_main_color.css">
+
   </head>
 
   <header><!-- --></header>
@@ -15,12 +17,11 @@
     <button id=capteur_navbar><img class=image_navbar src="../../../../icons/navbar.png" alt="navbar wheel">
       <nav id=container_navbar>
         <ul>
-          <li class=navbar_content><a href="#">Tableau de bord</a></li>
-          <li class=navbar_content><a href="#">test</a></li>
-          <li class=navbar_content><a href="#">Base de données</a></li>
-          <li class=navbar_content><a href="#">Liste des utilisateurs</a></li>
-          <li class=navbar_content><a href="#">Paramètres</a></li>
-          <li class=navbar_content><a href="#">Déconnexion</a></li>
+          <li class=navbar_content><img class=navbar_content_image src="../../../../icons/dashboard.png"><div class=container_navbar_content_text><a class=navbar_content_text href="#">Tableau de bord</a></div></li>
+          <li class=navbar_content><img class=navbar_content_image src="../../../../icons/database.png"><div class=container_navbar_content_text><a class=navbar_content_text href="#">Base de données</a></div></li>
+          <li class=navbar_content><img class=navbar_content_image src="../../../../icons/userlist.png"><div class=container_navbar_content_text><a class=navbar_content_text href="#">Liste des utilisateurs</a></div></li>
+          <li class=navbar_content><img class=navbar_content_image src="../../../../icons/settings.png"><div class=container_navbar_content_text><a class=navbar_content_text href="#">Paramètres</a></div></li>
+          <li class=navbar_content><img class=navbar_content_image src="../../../../icons/logout.png"><div class=container_navbar_content_text><a class=navbar_content_text href="#">Déconnexion</a></div></li>
         </ul>
     <!-- navigation bar -->
       </nav>
@@ -28,17 +29,17 @@
     </div>
 
     <div>
-      <nav>
-        <ul>
-          <li><a href="#">Liste des pauses lectures à corriger</a></li>
-          <li><a href="#">Pause/lecture publiques</a></li>
-          <li><a href="#">Pause/lecture non publiques</a></li>
-          <li><a href="#">Publier un message (pour les autres élèves)</a></li>
+      <nav id=container_middle_navbar>
+        <ul id=middle_navbar>
+          <li class=middle_navbar_content><a class=middle_navbar_content_text href="#">Liste des pauses lectures à corriger</a></li>
+          <li class=middle_navbar_content><a class=middle_navbar_content_text href="#">Pause/lecture publiques</a></li>
+          <li class=middle_navbar_content><a class=middle_navbar_content_text href="#">Pause/lecture non publiques</a></li>
+          <li class=middle_navbar_content><a class=middle_navbar_content_text href="#">Publier un message (pour les autres élèves)</a></li>
         </ul>
       </nav>
     </div>
 
-    <div><a href="#">Mon profil</a></div>
+    <div id=container_profil><img class=navbar_content_image src="../../../../icons/profil.png"><a href="#">Mon profil</a></div>
 
     <div id=container_footer><?php require_once('footer.html'); ?></div>
 
--- site/desktop/view/Public/defaultViewPublic.php
+++ site/desktop/view/Public/defaultViewPublic.php
@@ -4,25 +4,23 @@
     <meta charset="utf-8">
     <title>Accueil</title>
     <link rel="stylesheet" href="../../config/stylesheet_desktop/theme_main.css">
-    <!--<link rel="stylesheet" href="../config/stylesheet_desktop/theme_position.css">-->
+    <link rel="stylesheet" href="../../config/stylesheet_desktop/theme_main_color.css">
   </head>
 
   <body id=container_body>
   <!-- top of page -->
   <div>
-    <button id=capteur_navbar><img class=image_navbar src="../../../../icons/navbar.png" alt="navbar wheel">
-      <nav id=container_navbar>
-        <ul>
-          <li class=navbar_content><a href="#">Liste des pauses lectures à corriger</a></li>
-          <li class=navbar_content><a href="#">Pause/lecture publiques</a></li>
-          <li class=navbar_content><a href="#">Pause/lecture non publiques</a></li>
-          <li class=navbar_content><a href="#">Publier un message (pour les autres élèves)</a></li>
+      <nav id=container_middle_navbar>
+        <ul id=middle_navbar>
+          <li class=middle_navbar_content><a class=middle_navbar_content_text href="#">Liste des pauses lectures à corriger</a></li>
+          <li class=middle_navbar_content><a class=middle_navbar_content_text href="#">Pause/lecture publiques</a></li>
+          <li class=middle_navbar_content><a class=middle_navbar_content_text href="#">Pause/lecture non publiques</a></li>
+          <li class=middle_navbar_content><a class=middle_navbar_content_text href="#">Publier un message (pour les autres élèves)</a></li>
         </ul>
       </nav>
-    </button>
   </div>
 
-  <div id=profil><a href="#">Mon profil</a></div>
+  <div id=container_profil><img class=navbar_content_image src="../../../../icons/profil.png"><a href="#">Mon profil</a></div>
 
   <div id=container_footer><?php require_once('footer.html'); ?></div>
 
