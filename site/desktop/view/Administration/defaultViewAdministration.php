61f77d56beac9702cacf56748e7a4d390a1c0c4e
Shuk029920 <arthurtesx@gmail.com>
2020-06-21 22:43:39 +0200

    ee

--- site/desktop/view/Administration/defaultViewAdministration.php
+++ site/desktop/view/Administration/defaultViewAdministration.php
@@ -3,9 +3,9 @@
   <head>
     <meta charset="utf-8">
     <title>Accueil</title>
-    <link rel="stylesheet" media="screen and (max-width: 1280px)" href="../../config/stylesheet_desktop/theme_main_720p.css">
-    <link rel="stylesheet" media="screen and (min-width: 1280px)" href="../../config/stylesheet_desktop/theme_main_900p.css">
-    <link rel="stylesheet" media="screen and (min-width: 1600px)" href="../../config/stylesheet_desktop/theme_main_1080p.css">
+    <link rel="stylesheet" media="screen and (max-width: 1280px)" href="../config/stylesheet_desktop/theme_main_720p.css">
+    <link rel="stylesheet" media="screen and (min-width: 1280px)" href="../config/stylesheet_desktop/theme_main_900p.css">
+    <link rel="stylesheet" media="screen and (min-width: 1600px)" href="../config/stylesheet_desktop/theme_main_1080p.css">
 
   </head>
 
@@ -15,14 +15,14 @@
   <body id=container_body>
 
     <div>
-    <button id=capteur_navbar><img class=image_navbar src="../../../../icons/navbar.png" alt="navbar wheel">
+    <button id=capteur_navbar><img class=image_navbar src="../../../icons/navbar.png" alt="navbar wheel">
       <nav id=container_navbar>
         <ul>
-          <li class=navbar_content><img class=navbar_content_image src="../../../../icons/dashboard.png"><div class=container_navbar_content_text><a class=navbar_content_text href="#">Tableau de bord</a></div></li>
-          <li class=navbar_content><img class=navbar_content_image src="../../../../icons/database.png"><div class=container_navbar_content_text><a class=navbar_content_text href="#">Base de données</a></div></li>
-          <li class=navbar_content><img class=navbar_content_image src="../../../../icons/userlist.png"><div class=container_navbar_content_text><a class=navbar_content_text href="#">Liste des utilisateurs</a></div></li>
-          <li class=navbar_content><img class=navbar_content_image src="../../../../icons/settings.png"><div class=container_navbar_content_text><a class=navbar_content_text href="#">Paramètres</a></div></li>
-          <li class=navbar_content><img class=navbar_content_image src="../../../../icons/logout.png"><div class=container_navbar_content_text><a class=navbar_content_text href="#">Déconnexion</a></div></li>
+          <li class=navbar_content><img class=navbar_content_image src="../../../icons/dashboard.png"><div class=container_navbar_content_text><a class=navbar_content_text href="#">Tableau de bord</a></div></li>
+          <li class=navbar_content><img class=navbar_content_image src="../../../icons/database.png"><div class=container_navbar_content_text><a class=navbar_content_text href="#">Base de données</a></div></li>
+          <li class=navbar_content><img class=navbar_content_image src="../../../icons/userlist.png"><div class=container_navbar_content_text><a class=navbar_content_text href="#">Liste des utilisateurs</a></div></li>
+          <li class=navbar_content><img class=navbar_content_image src="../../../icons/settings.png"><div class=container_navbar_content_text><a class=navbar_content_text href="#">Paramètres</a></div></li>
+          <li class=navbar_content><img class=navbar_content_image src="../../../icons/logout.png"><div class=container_navbar_content_text><a class=navbar_content_text href="#">Déconnexion</a></div></li>
         </ul>
     <!-- navigation bar -->
       </nav>
@@ -32,15 +32,15 @@
     <div>
       <nav id=container_middle_navbar>
         <ul id=middle_navbar>
-          <li class=middle_navbar_content  style="border-left-style: solid;"><a class=middle_navbar_content_text href="#">Liste des pauses lectures à corriger</a></li>
-          <li class=middle_navbar_content><a class=middle_navbar_content_text href="#">Pause/lecture publiques</a></li>
-          <li class=middle_navbar_content><a class=middle_navbar_content_text href="#">Pause/lecture non publiques</a></li>
-          <li class=middle_navbar_content><a class=middle_navbar_content_text href="#">Publier un message (pour les autres élèves)</a></li>
+          <li class=middle_navbar_content  style="border-left-style: solid;"><a class=middle_navbar_content_text href="#">Pauses à corriger</a></li>
+          <li class=middle_navbar_content><a class=middle_navbar_content_text href="#">Pauses publiques</a></li>
+          <li class=middle_navbar_content><a class=middle_navbar_content_text href="#">Pauses non publiques</a></li>
+          <li class=middle_navbar_content><a class=middle_navbar_content_text href="#">Publier un message</a></li>
         </ul>
       </nav>
     </div>
 
-    <div id=container_profil><img class=navbar_content_image src="../../../../icons/profil.png"><a href="#">Mon profil</a></div>
+    <div id=container_profil><img class=navbar_content_image src="../../../icons/profil.png"><a href="#">Mon profil</a></div>
 
     <div id=container_footer><?php require_once('../view/footer.html'); ?></div>
 
--- site/desktop/view/Public/defaultViewPublic.php
+++ site/desktop/view/Public/defaultViewPublic.php
@@ -3,9 +3,9 @@
   <head>
     <meta charset="utf-8">
     <title>Accueil</title>
-    <link rel="stylesheet" media="screen and (max-width: 1280px)" href="../../config/stylesheet_desktop/theme_main_720p.css">
-    <link rel="stylesheet" media="screen and (min-width: 1280px)" href="../../config/stylesheet_desktop/theme_main_900p.css">
-    <link rel="stylesheet" media="screen and (min-width: 1600px)" href="../../config/stylesheet_desktop/theme_main_1080p.css">
+    <link rel="stylesheet" media="screen and (max-width: 1280px)" href="../config/stylesheet_desktop/theme_main_720p.css">
+    <link rel="stylesheet" media="screen and (min-width: 1280px)" href="../config/stylesheet_desktop/theme_main_900p.css">
+    <link rel="stylesheet" media="screen and (min-width: 1600px)" href="../config/stylesheet_desktop/theme_main_1080p.css">
   </head>
 
   <body id=container_body>
@@ -21,7 +21,7 @@
       </nav>
   </div>
 
-  <div id=container_profil><img class=navbar_content_image src="../../../../icons/profil.png"><a href="#">Mon profil</a></div>
+  <div id=container_profil><img class=navbar_content_image src="../../../icons/profil.png"><a href="#">Mon profil</a></div>
 
   <div id=container_footer><?php require_once('../view/footer.html'); ?></div>
 
