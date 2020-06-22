bf5b8cb1e284acf6e4bf6cda626c317a3ec5535a
Shuk029920 <arthurtesx@gmail.com>
2020-06-22 22:52:23 +0200

    update 99

--- site/desktop/functions/logs.php
+++ site/desktop/functions/logs.php
@@ -3,14 +3,16 @@ function connect_logs()
 {
     $file = fopen('logs.txt', 'a+');
     $now = date("F j, Y, g:i a");
-    fwrite($file, 'User : '.$_SESSION['username'].' => '.'Status : ' .$_SESSION['status'].' => '. 'Heure de connexion : '. $now . PHP_EOL);
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    fwrite($file, $ip .' => User : '.$_SESSION['username'].' => '.'Status : ' .$_SESSION['status'].' => '. 'Heure de connexion : '. $now . PHP_EOL);
     fclose($file);
 }
 function deconnect_logs()
 {
   $file = fopen('../controller/logs.txt', 'a+');
   $now = date("F j, Y, g:i a");
-  fwrite($file, 'User : '.$_SESSION['username'].' => '.'Status : ' .$_SESSION['status'].' => '. 'Heure de déconnexion : '. $now . PHP_EOL);
+  $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+  fwrite($file, $ip . ' => User : '.$_SESSION['username'].' => '.'Status : ' .$_SESSION['status'].' => '. 'Heure de déconnexion : '. $now . PHP_EOL);
   fclose($file);
 }
 ?>
