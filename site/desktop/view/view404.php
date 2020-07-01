baf5c1aad6e3bf5914ee4eb2e9b22f22a2ffe0ac
darkshark400 <51637671+darkshark400@users.noreply.github.com>
2020-07-02 00:43:03 +0200

    sd

--- site/desktop/view/view404.php
+++ site/desktop/view/view404.php
@@ -3,15 +3,13 @@
   <head>
     <meta charset="utf-8">
     <title>404</title>
-    <link rel="stylesheet" media="screen and (max-width: 1280px)" href="/site/desktop/config/stylesheet_desktop/theme_main_720p.css">
-    <link rel="stylesheet" media="screen and (min-width: 1280px)" href="/site/desktop/config/stylesheet_desktop/theme_main_900p.css">
-    <link rel="stylesheet" media="screen and (min-width: 1600px)" href="/site/desktop/config/stylesheet_desktop/theme_main_1080p.css">
+    <link rel="stylesheet" href="/site/desktop/config/stylesheet_desktop/theme_main.css">
   </head>
 
   <body id="container_body">
     <div>
       <div class=erreur_404>ERREUR 404 : la page recherchée n'existe pas ...</div>
-      <img class=image_404 src='/icons/404/404.gif'>
+      <img id=image_404 src='/icons/404/404.gif'>
     </div>
     <div id=container_footer><?php require_once($root.'site/desktop/view/viewFooter.html'); ?></div>
   </body>
