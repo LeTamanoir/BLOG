58c6aaf5be183d4fc31e8ba047f0669b7cd70632
darkshark400 <51637671+darkshark400@users.noreply.github.com>
2020-07-01 18:07:02 +0200

    test

--- site/desktop/view/Administration/viewDefaultAdministration.php
+++ site/desktop/view/Administration/viewDefaultAdministration.php
@@ -18,16 +18,17 @@
           <li class=navbar_content><img class=navbar_content_image src="/icons/navbar/100px/database.png"><div class=container_navbar_content_text><a class=navbar_content_text onclick="showIframe(5);" href="#">Base de données</a></div></li>
           <li class=navbar_content><img class=navbar_content_image src="/icons/navbar/100px/user_list.png"><div class=container_navbar_content_text><a class=navbar_content_text onclick="showIframe(6);" href="#">Liste des utilisateurs</a></div></li>
           <li class=navbar_content><img class=navbar_content_image src="/icons/navbar/100px/settings.png"><div class=container_navbar_content_text><a class=navbar_content_text onclick="showIframe(7);" href="#">Paramètres</a></div></li>
+          <li class=navbar_content><img class=navbar_content_image src="/icons/navbar/100px/user.png"><div class=container_navbar_content_text><a class=navbar_content_text onclick="showIframe(8);" href="#">Mon profil</a></div></li>
           <li class=navbar_content><img class=navbar_content_image src="/icons/navbar/100px/logout.png"><div class=container_navbar_content_text><a class=navbar_content_text href="/site/desktop/functions/logout.php">Déconnexion</a></div></li>
         </ul>
       </nav>
     </div>
     <nav id=container_middle_navbar>
       <ul id=middle_navbar>
-        <li class=middle_navbar_content><a class=middle_navbar_content_text onclick="showIframe(0);" href="#">Pauses à corriger</a></li>
-        <li class=middle_navbar_content><a class=middle_navbar_content_text onclick="showIframe(1);" href="#">Pauses publiques</a></li>
-        <li class=middle_navbar_content><a class=middle_navbar_content_text onclick="showIframe(2);" href="#">Pauses non publiques</a></li>
-        <li class=middle_navbar_content><a class=middle_navbar_content_text href="#" onclick="showIframe(3);">Publier un message</a></li>
+        <li class=middle_navbar_content><img class=middle_navbar_content_image src="/icons/navbar/100px/correct.png"><a class=middle_navbar_content_text onclick="showIframe(0);" href="#">Pauses à corriger</a></li>
+        <li class=middle_navbar_content><img class=middle_navbar_content_image src="/icons/navbar/100px/entry.png"><a class=middle_navbar_content_text onclick="showIframe(1);" href="#">Pauses publiques</a></li>
+        <li class=middle_navbar_content><img class=middle_navbar_content_image src="/icons/navbar/100px/no_entry.png"><a class=middle_navbar_content_text onclick="showIframe(2);" href="#">Pauses non publiques</a></li>
+        <li class=middle_navbar_content><img class=middle_navbar_content_image src="/icons/navbar/100px/message.png"><a class=middle_navbar_content_text href="#" onclick="showIframe(3);">Publier un message</a></li>
       </ul>
     </nav>
     <?php if(isset($_COOKIE['background']))
