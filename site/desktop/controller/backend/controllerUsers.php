ab1ada1c4fe17e6b97d920efd132be0563ab7ef3
Shuk029920 <arthurtesx@gmail.com>
2020-07-02 21:31:50 +0200

    sz

--- site/desktop/controller/backend/controllerUsers.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$root = "/kunden/homepages/14/d285337155/www/Arthur/sites/blog/";
-require_once($root.'site/desktop/config/conf.php');
-require_once($root.'site/desktop/model/backend/modelUsers.php');
-require_once($root.'site/desktop/view/Administration/viewUsers.php');
-?>
