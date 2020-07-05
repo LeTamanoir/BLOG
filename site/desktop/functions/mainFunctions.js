1ba4e4d1af703aaf8e5ee39310c1110f236f674f
darkshark400 <51637671+darkshark400@users.noreply.github.com>
2020-07-05 14:05:48 +0200

    gd

--- site/desktop/functions/mainFunctions.js
+++ site/desktop/functions/mainFunctions.js
@@ -17,7 +17,13 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/service-worker.js');
   });
 }
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
 
 
 // ===================== newPdp ===================== //
