5ab7e77b82b2d385f9030f16a36e56123075e0ee
darkshark400 <51637671+darkshark400@users.noreply.github.com>
2020-07-09 03:43:49 +0200

    update

--- pages/templates/default.php
+++ pages/templates/default.php
@@ -10,40 +10,42 @@
 <body id=container__body>
 
     <nav id="navbar">
-        <input id=navbar__capteur class="image_big" type="image" src="/public/icons/navbar/100px/navbar.png">
-
+        <div class="flex_row border_bottom">
+            <title id=navbar__title class="color_dark_blue text_big center_block">BLOG Prototype</title>
+            <input id=navbar__capteur class="image_big" type="image" src="/public/icons/navbar/navbar.png">
+        </div>
         <div id="navbar__content">
             <div class="navbar__content__child margin_top padding_small">
-                <img class="image_medium padding_right" src="/public/icons/navbar/100px/dashboard.png" alt="dashboard"><a href=# class="text_medium center color_black underline_hover">dashboard</a>
+                <img class="image_medium padding_right" src="/public/icons/navbar/dashboard.png" alt="dashboard"><a href=# class="text_medium center_block color_black underline_hover">dashboard</a>
             </div>
             <div class="navbar__content__child margin_top padding_small">
-                <img class="image_medium padding_right" src="/public/icons/navbar/100px/database.png" alt="database"><a href=# class="text_medium center color_black underline_hover">database</a>
+                <img class="image_medium padding_right" src="/public/icons/navbar/database.png" alt="database"><a href=# class="text_medium center_block color_black underline_hover">database</a>
             </div>
             <div class="navbar__content__child margin_top padding_small">
-                <img class="image_medium padding_right" src="/public/icons/navbar/100px/user_list.png" alt="user list"><a href=# class="text_medium center color_black underline_hover">user list</a>
+                <img class="image_medium padding_right" src="/public/icons/navbar/user_list.png" alt="user list"><a href=# class="text_medium center_block color_black underline_hover">user list</a>
             </div>
             <div class="navbar__content__child margin_top padding_small">
-                <img class="image_medium padding_right" src="/public/icons/navbar/100px/user.png" alt="user"><a href=# class="text_medium center color_black underline_hover">user</a>
+                <img class="image_medium padding_right" src="/public/icons/navbar/user.png" alt="user"><a href=# class="text_medium center_block color_black underline_hover">user</a>
             </div>
             <div class="navbar__content__child margin_top padding_small">
-                <img class="image_medium padding_right" src="/public/icons/navbar/100px/settings.png" alt="settings"><a href=# class="text_medium center color_black underline_hover">settings</a>
+                <img class="image_medium padding_right" src="/public/icons/navbar/settings.png" alt="settings"><a href=# class="text_medium center_block color_black underline_hover">settings</a>
             </div>
             <div class="navbar__content__child margin_top padding_small">
-                <img class="image_medium padding_right" src="/public/icons/navbar/100px/logout.png" alt="logout"><a href=# class="text_medium center color_black underline_hover">logout</a>
+                <img class="image_medium padding_right" src="/public/icons/navbar/logout.png" alt="logout"><a href=# class="text_medium center_block color_black underline_hover">logout</a>
             </div>
         </div>
     </nav>  
     <main id="main">
-        <header id="header">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat adipisci architecto, repudiandae explicabo accusantium sint omnis atque consequatur, libero iure incidunt possimus minus deleniti? Deserunt facilis incidunt unde eius velit?
+        <header id="header" class="text_center">
+            this is header
         </header>
         
-        <div id="content">
+        <div id="content" class="center_block">
             <?= $content; ?>
         </div>
 
-        <footer id="footer">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat adipisci architecto, repudiandae explicabo accusantium sint omnis atque consequatur, libero iure incidunt possimus minus deleniti? Deserunt facilis incidunt unde eius velit?
+        <footer id="footer" class="text_center">
+            this is footer
         
         </footer>
     </main>
--- public/js/mainFunctions.js
+++ public/js/mainFunctions.js
@@ -8,29 +8,65 @@ window.onload = () => {
  console.log("hello world !")
   
   
-  
-  
-  
-document.getElementById("navbar__capteur").addEventListener("click", show_navbar);
-  
-  
+ 
   
 navbar__status = 1
-function show_navbar() {
+function show_navbar_desktop() {
   if (navbar__status==1)
   {
     navbar__status = 0;
-    document.getElementById("navbar").style.animation = "navbar__in 0.5s forwards";
-    document.getElementById("navbar__capteur").src = "/public/icons/navbar/100px/cross.png";
+    document.getElementById("navbar").style.animation = "navbar__desktop__in 0.1s forwards";
+    document.getElementById("navbar__capteur").src = "/public/icons/navbar/cross.png";
 
   }
   else if (navbar__status==0)
   {
     navbar__status = 1;
-    document.getElementById("navbar").style.animation = "navbar__out 0.5s forwards";
-    document.getElementById("navbar__capteur").src = "/public/icons/navbar/100px/navbar.png";
+    document.getElementById("navbar").style.animation = "navbar__desktop__out 0.1s forwards";
+    document.getElementById("navbar__capteur").src = "/public/icons/navbar/navbar.png";
 
 
   }
 }
-  
+function show_navbar_mobile() {
+  if (navbar__status==1)
+  {
+    navbar__status = 0;
+    document.getElementById("navbar__content").style.animation = "navbar__mobile__in 0.4s forwards";
+    document.getElementById("navbar__capteur").src = "/public/icons/navbar/cross.png";
+  }
+  else if (navbar__status==0)
+  {
+    navbar__status = 1;
+    document.getElementById("navbar__content").style.animation = "navbar__mobile__out 0.1s forwards";
+    document.getElementById("navbar__capteur").src = "/public/icons/navbar/navbar.png";
+
+
+  }
+}
+screenSizeStatus2 =''
+function screenSize(x) {
+  if (x.matches) {
+    document.getElementById("navbar__capteur").addEventListener("click", show_navbar_mobile);
+    screenSizeStatus = 1
+    console.log(screenSizeStatus)
+  }
+  else {
+    document.getElementById("navbar__capteur").addEventListener("click", show_navbar_desktop);
+    screenSizeStatus = 0
+    console.log(screenSizeStatus)
+  }
+  if (screenSizeStatus == 1 && screenSizeStatus2 == 1)
+  {
+    location.reload();
+  }
+  else if (screenSizeStatus == 0 && screenSizeStatus2 == 1)
+  {
+    location.reload();
+  }
+  screenSizeStatus2 = 1
+}
+
+var x = window.matchMedia("(max-width: 1000px)")
+screenSize(x)
+x.addListener(screenSize)
