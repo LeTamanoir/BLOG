4306a56e5b6c4bfdc1e7cac922224819e7e86166
darkshark400 <51637671+darkshark400@users.noreply.github.com>
2020-07-14 02:43:16 +0200

    lol1

--- app/Views/templates/users/default.php
+++ app/Views/templates/users/default.php
@@ -10,43 +10,56 @@
 </head>
 
 <body id=container__body>
-
     <nav id="navbar">
         <div id="navbar__container">
         <div class="flex_row">
-            <title id=navbar__title class="color_dark_blue text_big center_block">BLOG Prototype</title>
-            <input class="shapeshifter cursor" id="navbar__capteur" type="button">
+            <p class="color_dark_blue show_mobile text_big center_block">BLOG Prototype</p>
+            <button class="shapeshifter cursor margin_x_small" id="navbar__capteur" type="button"></button>
         </div>
         <div id="navbar__content">
-            <div class="navbar__content__child padding_right">
-                <div class="padding_horizontal_navbar"><input id=user class="shapeshifter play"  type="button"></div><a href=# class="text_medium padding_x_small center_block color_black underline_hover">user</a>
-            </div>
-            <div class="navbar__content__child padding_right">
-                <div class="padding_horizontal_navbar"><input id=settings class="shapeshifter play"  type="button"></div><a href=# class="text_medium padding_x_small center_block color_black underline_hover">settings</a>
-            </div>
-            <div class="navbar__content__child padding_right">
-                <div class="padding_horizontal_navbar"><input id=logout class="shapeshifter play"  type="button"></div><a href=# class="text_medium padding_x_small center_block color_black underline_hover">logout</a>
-            </div>
+            <a class="navbar__content__child padding_x_small cursor" href="#">
+                <div id=dashboard class="shapeshifter play margin_horizontal"></div><p class="text_medium text_navbar text_center padding_x_small center_block color_black underline_hover">dashboard</p>
+            </a>
+            <a class="navbar__content__child padding_x_small cursor" href="#">
+                <div id=user class="shapeshifter play margin_horizontal" ></div><p class="text_medium text_navbar text_center padding_x_small center_block color_black underline_hover">user</p>
+            </a>
+            <a class="navbar__content__child padding_x_small cursor" href="#">
+                <div id=settings class="shapeshifter play margin_horizontal" ></div><p class="text_medium text_navbar text_center padding_x_small center_block color_black underline_hover">settings</p>
+            </a>
+            <a class="navbar__content__child padding_x_small cursor" href="#">
+                <div id=logout class="shapeshifter play margin_horizontal" ></div><p class="text_medium text_navbar text_center padding_x_small center_block color_black underline_hover">logout</p>
+            </a>
         </div>
         </div>
-    </nav>  
-    <main id="main" class=background_color_white>
+    </nav>
+    <main id="main" class="">
         <header id="header" class="text_center flex_row">
             <p class="text_big color_black show_desktop">BLOG Prototype | </p><a href="index.php" class="text_big color_black inline underline_hover">Accueil</a>
         </header>
         
-        <div id="content" class="background_color_white">
+        <div id="content" class="">
             <div class="padding_small">
-                <?php $content ?>
+                <?= $content; ?>
             </div>
         </div>
 
     </main>
-    <footer id="footer" class="flex_column background_color_light_grey">
+    <footer id="footer" class="flex_column ">
         <div class=padding_x_small>
             <p class="text_small">this is footer</p>
         </div>
     </footer>
+    <div id=container_background>
+        <div id=background_desktop>
+            <svg xmlns="http://www.w3.org/2000/svg" class=navbar_background id=navbar_background_1><defs></defs><title>navbar_background_1</title><g id="Layer_2_navbar" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path d="M197.56,0c4.21,9,31.44,69.38-1.3,125.28-22.8,38.91-62.26,54.48-84.48,63.25C63.83,207.44,20.46,204.54,0,201.91V0Z"/></g></g></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" class=navbar_background id=navbar_background_2><defs></defs><title>navbar_background_2</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path d="M0,1018c361.65,33.61,472.56-18,502.86-75.57,23.8-45.21-13.58-72.33,0-178.42,18.12-141.59,93.2-159.72,84-261.33-8.82-97.44-80.14-105.63-74.68-192.06,6.14-97.22,99.08-130.29,85.18-197.31C590.34,79.5,556.17,37.44,438.69,0H0Z"/></g></g></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" class=navbar_background id=navbar_background_3><defs></defs><title>navbar_background_3</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path d="M1409,0c23.26,79.82,61.69,254.68,0,456C1306.43,790.72,997.77,935,902,980c-164.36,77.31-314.61,95.61-406,100H0V0Z"/></g></g></svg>
+        </div>
+        <div id="background_mobile">
+            <svg xmlns="http://www.w3.org/2000/svg" class=navbar_background id=navbar_background_1_mobile><defs></defs><title>navbar_background_1_mobile</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><g id="Layer_1-2-2" data-name="Layer 1-2"><path d="M11.92,0C-25.67,260.89,32,340.93,96.4,362.8c50.6,17.17,80.86-9.8,199.46,0C454.15,375.87,474.43,430,588,423.4c108.94-6.33,118.1-57.8,214.73-53.86C911.38,374,948.35,441,1023.28,431c37.84-5.07,84.87-29.72,126.72-114.48V0Z"/></g></g></g></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" class=navbar_background id=navbar_background_2_mobile><defs></defs><title>navbar_background_2_mobile</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><g id="Layer_1-2-2" data-name="Layer 1-2"><path d="M1150,781.5c-85,12.9-271.19,34.22-485.55,0-356.42-56.89-510-228.09-558-281.21C24.15,409.13,4.67,325.8,0,275.11V0H1150Z"/></g></g></g></svg>
+        </div>
+    </div>
     <script src="/public/js/mainFunctions.js"></script>
 </body>
 </html>
--- public/js/mainFunctions.js
+++ public/js/mainFunctions.js
@@ -1,17 +1,272 @@
 window.onload = () => {
   'use strict';
-  console.log("hello world !")
   
   //if ('serviceWorker' in navigator) {
   //  navigator.serviceWorker.register('/sw.js');
   //}
 }
-console.log("hello world !")
- 
+
+
+//===================================
+//                                 ||
+//    ------  INDEX  ------        ||
+//                                 ||
+//    -- 1 -- CHECK ELEMENTS       ||
+//                                 ||
+//    -- 2 -- DEFINE ELEMENTS      ||
+//                                 ||
+//    -- 3 -- LOGIN                ||
+//                                 ||
+//    -- 4 -- NAVBAR               ||
+//                                 ||
+//===================================
+
+
+
+// ==========================
+// ===== CHECK ELEMENTS =====
+// ==========================
+
+
+// =====================================
+// ===== CHECK ELEMENTS ---> checkNavbar
+// =====================================
+
+
+var checkNavbar = document.getElementById("navbar");
+if(typeof(checkNavbar) != 'undefined' && checkNavbar != null){
+  navbarExist = true;
+}
+else{
+  navbarExist = false;
+}
+
+
+// ====================================
+// ===== CHECK ELEMENTS ---> checkLogin
+// ====================================
+
+
+var checkLogin = document.getElementById("container_logo_login");
+
+if(typeof(checkLogin) != 'undefined' && checkLogin != null){
+  loginExist = true;
+}
+else{
+  loginExist = false;
+}
+
+
+// ====================================
+// ===== CHECK ELEMENTS ---> checkAdmin
+// ====================================
+
+
+var checkAdmin = document.getElementById("database");
+
+if(typeof(checkAdmin) != 'undefined' && checkAdmin != null){
+  adminExist = true;
+}
+else{
+  adminExist = false;
+}
+
+
+
+// ===========================
+// ===== DEFINE ELEMENTS =====
+// ===========================
+
+
+// ==========================================
+// ===== DEFINE ELEMENTS ---> CheckScreenSize
+// ==========================================
+
+
+const screen_res_mobile = window.matchMedia("(max-width: 1150px)");
+const screen_res_laptop = window.matchMedia("(min-width: 1150px) and (max-width: 1500px)");
+const screen_res_desktop = window.matchMedia("(min-width: 1500px)");
+screen_mobile(screen_res_mobile);
+screen_laptop(screen_res_laptop);
+screen_desktop(screen_res_desktop);
+screen_res_mobile.addListener(screen_mobile);
+screen_res_laptop.addListener(screen_laptop);
+screen_res_desktop.addListener(screen_desktop);
+
+
+// ========================================
+// ===== DEFINE ELEMENTS ---> screen_mobile
+// ========================================
+
+
+function screen_mobile(screen){
+  if(screen.matches) {
+    if (navbarExist==true) {
+      document.getElementById("navbar__capteur").addEventListener("click", show_navbar_mobile);
+    }
+    screen_format = 0;
+    console.log("mobile")
+    defineElements();
+
+  }
+}
+
+
+// ========================================
+// ===== DEFINE ELEMENTS ---> screen_laptop
+// ========================================
+
+
+function screen_laptop(screen){
+  if(screen.matches) {
+    if (navbarExist==true) {
+      document.getElementById("navbar__capteur").addEventListener("click", show_navbar_desktop);
+    }
+
+    screen_format = 1
+    console.log("laptop")
+    defineElements();
+    window.addEventListener('resize', relaodScreen);
+  }
+}
+
+
+// =========================================
+// ===== DEFINE ELEMENTS ---> screen_desktop
+// =========================================
+
+
+function screen_desktop(screen){
+  if(screen.matches) {
+    if (navbarExist==true) {
+      document.getElementById("navbar__capteur").addEventListener("click", show_navbar_desktop);
+    }
+
+    screen_format = 1
+    console.log("desktop");
+    defineElements();
+    window.addEventListener('resize', relaodScreen);
+  }
+}
+
+
+// =======================================
+// ===== DEFINE ELEMENTS ---> relaodScreen
+// =======================================
+
+
+function relaodScreen() {
+  location.reload();
+}
+
+
+// =========================================
+// ===== DEFINE ELEMENTS ---> defineElements
+// =========================================
+
+
+function defineElements() {
+  if (loginExist==true) {
+    document.getElementById("container_logo_login").style.backgroundImage = "url(/public/icons/login/blog.svg)";
+    document.getElementById("container_logo_login").addEventListener("mouseover", async function() {
+      document.getElementById("container_logo_login").style.animationName = "play_logo_reverse";
+      await new Promise(r => setTimeout(r, 500));
+      document.getElementById("container_logo_login").style.animationName = "play_logo";
+    });
+  }
+  if (navbarExist==true) {
+    document.getElementById("navbar__capteur").style.backgroundImage = "url(/public/icons/navbar/animated_svg/navbar.svg)";
+    document.getElementById("user").style.backgroundImage = "url(/public/icons/navbar/animated_svg/user.svg)";
+    document.getElementById("dashboard").style.backgroundImage = "url(/public/icons/navbar/animated_svg/dashboard.svg)";
+    document.getElementById("settings").style.backgroundImage = "url(/public/icons/navbar/animated_svg/settings.svg)";
+    document.getElementById("logout").style.backgroundImage = "url(/public/icons/navbar/animated_svg/logout.svg)";
+    document.getElementById("user").addEventListener("mouseover", animate);
+    document.getElementById("settings").addEventListener("mouseover", animate);
+    document.getElementById("logout").addEventListener("mouseover", animate);
+    document.getElementById("dashboard").addEventListener("mouseover", animate);
+  }
+  if (adminExist==true) {
+    document.getElementById("user_list").style.backgroundImage = "url(/public/icons/navbar/animated_svg/user_list.svg)";
+    document.getElementById("database").style.backgroundImage = "url(/public/icons/navbar/animated_svg/database.svg)";
+    document.getElementById("user_list").addEventListener("mouseover", animate);
+    document.getElementById("database").addEventListener("mouseover", animate);
+  } 
+};
 
 
 
 
+
+
+// ==================
+// ===== LOGIN ======
+// ==================
+
+
+// ======================
+// ===== LOGIN ---> LOGIN
+// ======================
+
+eye_status_1 = "open";
+eye_status_2 = "open";
+function switch_eye(x) {
+  var name ="image_eye_"+x;
+  if (x=="1")
+  {
+    if (eye_status_1=="open")
+    {
+      document.getElementById(name).src = "/public/icons/login/no_eye.png";
+      eye_status_1 = "close";
+    }
+    else if (eye_status_1=="close")
+    {
+      document.getElementById(name).src = "/public/icons/login/eye.png";
+      eye_status_1 = "open";
+    }
+  }
+  else if (x=="2")
+  {
+    if (eye_status_2=="open")
+    {
+      document.getElementById(name).src = "/public/icons/login/no_eye.png";
+      eye_status_2 = "close";
+    }
+    else if (eye_status_2=="close")
+    {
+      document.getElementById(name).src = "/public/icons/login/eye.png";
+      eye_status_2 = "open";
+    }
+  }
+}
+
+
+// ========================
+// ===== LOGIN ---> seePass
+// ========================
+
+
+function seePass(x){
+  var pwd = document.getElementById('password'+x);
+  if(pwd.type == 'password')
+  {
+    pwd.type = 'text'
+  }
+  else
+  {
+    pwd.type = 'password';
+  }
+}
+
+
+
+// ==================
+// ===== NAVBAR =====
+// ==================
+
+
+// ====================================
+// ===== NAVBAR --> show_navbar_desktop
+// ====================================
 
 
 navbar__status = 1
@@ -21,7 +276,6 @@ function show_navbar_desktop() {
     navbar__status = 0;
     document.getElementById("navbar__content").style.transform = "scale(1)";
     document.getElementById("navbar__capteur").style.transform = "translateX(175px)";
-
     document.getElementById("navbar_background_2").style.transform = "scale(1)";
     document.getElementById("navbar_background_2").style.fill = "var(--color-blue)";
     document.getElementById("container_background").style.zIndex = "1";
@@ -37,10 +291,15 @@ function show_navbar_desktop() {
     document.getElementById("container_background").style.zIndex = "-1";
     document.getElementById("navbar__capteur").style.transform = "translateX(0px)";
     animateNavbar("exit")
-
-
   }
 }
+
+
+// ====================================
+// ===== NAVBAR --> show_navbar_mobile
+// ====================================
+
+
 function show_navbar_mobile() {
   if (navbar__status==1)
   {
@@ -58,109 +317,57 @@ function show_navbar_mobile() {
     document.getElementById("navbar_background_1_mobile").style.transform = "scale(0)";
     document.getElementById("navbar_background_1_mobile").style.fill = "var(--color-light-blue)";
     document.getElementById("container_background").style.zIndex = "-1";
-
     animateNavbar("exit")
-
   }
 }
 
 
-
-const screen_res_mobile = window.matchMedia("(max-width: 1150px)");
-const screen_res_laptop = window.matchMedia("(min-width: 1150px) and (max-width: 1500px)");
-const screen_res_desktop = window.matchMedia("(min-width: 1500px)");
-
-screen_mobile(screen_res_mobile);
-screen_laptop(screen_res_laptop);
-screen_desktop(screen_res_desktop);
-screen_res_mobile.addListener(screen_mobile);
-screen_res_laptop.addListener(screen_laptop);
-screen_res_desktop.addListener(screen_desktop);
-
-function screen_mobile(screen){
-  if(screen.matches) {
-    try {
-      document.getElementById("navbar__capteur").addEventListener("click", show_navbar_mobile);
-    } catch (error) {}
-    screen_format = 0
-    console.log("mobile")
-    defineElements();
-
-  }
-}
-
-function screen_laptop(screen){
-  if(screen.matches) {
-    try {
-      document.getElementById("navbar__capteur").addEventListener("click", show_navbar_desktop);
-    } catch (error) {}
-
-    screen_format = 1
-    console.log("laptop")
-    defineElements();
-    window.addEventListener('resize', relaodScreen);
-    function relaodScreen() {
-      location.reload();
-    }
-  }
-}
-function screen_desktop(screen){
-  if(screen.matches) {
-    try {
-      document.getElementById("navbar__capteur").addEventListener("click", show_navbar_desktop);
-    } catch (error) {}
-
-    screen_format = 1
-    console.log("desktop");
-    defineElements();
-    window.addEventListener('resize', relaodScreen);
-    function relaodScreen() {
-      location.reload();
-    }
-  }
-}
+// ========================
+// ===== NAVBAR --> animate
+// ========================
 
 
+async function animate() {
+  this.classList.add('exit');
+  this.classList.remove('play');
 
-
-
-
-function defineElements() {
-  try {
-    document.getElementById("container_logo_login").style.backgroundImage = "url(/public/icons/login/blog.svg)";
-    document.getElementById("container_logo_login").addEventListener("mouseover", async function() {
-      document.getElementById("container_logo_login").style.animationName = "play_logo_reverse";
-      await new Promise(r => setTimeout(r, 500));
-      document.getElementById("container_logo_login").style.animationName = "play_logo";
-    });
-
-  } catch(error){}
-  try {
-    document.getElementById("navbar__capteur").style.backgroundImage = "url(/public/icons/navbar/animated_svg/navbar.svg)";
-    document.getElementById("user").style.backgroundImage = "url(/public/icons/navbar/animated_svg/user.svg)";
-    document.getElementById("settings").style.backgroundImage = "url(/public/icons/navbar/animated_svg/settings.svg)";
-    document.getElementById("logout").style.backgroundImage = "url(/public/icons/navbar/animated_svg/logout.svg)";
-    document.getElementById("user").addEventListener("mouseover", animate);
-    document.getElementById("settings").addEventListener("mouseover", animate);
-    document.getElementById("logout").addEventListener("mouseover", animate);
-    
-    
-  }catch (errot){}
-  try {
-    document.getElementById("user_list").style.backgroundImage = "url(/public/icons/navbar/animated_svg/user_list.svg)";
-    document.getElementById("dashboard").style.backgroundImage = "url(/public/icons/navbar/animated_svg/dashboard.svg)";
-    document.getElementById("database").style.backgroundImage = "url(/public/icons/navbar/animated_svg/database.svg)";
-    document.getElementById("user_list").addEventListener("mouseover", animate);
-    document.getElementById("dashboard").addEventListener("mouseover", animate);
-    document.getElementById("database").addEventListener("mouseover", animate);
-  }catch (error) {}
-  
-  
+  await new Promise(r => setTimeout(r, 500));
+  this.classList.remove('exit');
+  this.classList.add('play');
 };
 
 
+// ==============================
+// ===== NAVBAR --> retractNavbar
+// ==============================
 
 
+function retractNavbar() {
+  show_navbar_desktop()
+  animateNavbar("exit")
+}
+
+
+// ==============================
+// ===== NAVBAR --> animateNavbar
+// ==============================
+
+
+function animateNavbar(x) {
+  if (x=="exit") {
+    document.getElementById("navbar__capteur").classList.add('exit');
+    document.getElementById("navbar__capteur").classList.remove('play');
+  }
+  else if (x=="play") {
+    document.getElementById("navbar__capteur").classList.remove('exit');
+    document.getElementById("navbar__capteur").classList.add('play');  
+  }
+}
+
+
+// ========================================
+// ===== NAVBAR --> deployBackgroundDesktop
+// ========================================
 
 
 async function deployBackgroundDesktop () {
@@ -176,6 +383,13 @@ async function deployBackgroundDesktop () {
   document.getElementById("navbar_background_2").style.fill = "var(--color-light-blue)";
   retractNavbar()
 }
+
+
+// =======================================
+// ===== NAVBAR --> deployBackgroundMobile
+// =======================================
+
+
 async function deployBackgroundMobile () {
   document.getElementById("navbar_background_2_mobile").style.transform = "scale(1)";
   document.getElementById("navbar_background_2_mobile").style.fill = "var(--color-dark-blue)";
@@ -189,6 +403,12 @@ async function deployBackgroundMobile () {
   document.getElementById("navbar_background_1_mobile").style.fill = "var(--color-light-blue)";
   retractNavbar()
 }
+
+// =======================================
+// ===== NAVBAR --> deployBackgroundMobile
+// =======================================
+
+
 var navbar__content__child_lenght = document.getElementsByClassName('navbar__content__child');
 if (screen_format==1)
 {
@@ -201,86 +421,3 @@ else {
     document.getElementsByClassName("navbar__content__child")[i].addEventListener("click", deployBackgroundMobile);
   }
 }
-
-
-function retractNavbar() {
-  show_navbar_desktop()
-  animateNavbar("exit")
-}
-
-function animateNavbar(x) {
-  if (x=="exit") {
-    document.getElementById("navbar__capteur").classList.add('exit');
-    document.getElementById("navbar__capteur").classList.remove('play');
-  }
-  else if (x=="play") {
-    document.getElementById("navbar__capteur").classList.remove('exit');
-    document.getElementById("navbar__capteur").classList.add('play');  
-  }
-}
-
-
-
-
-
-eye_status_1 = "open";
-eye_status_2 = "open";
-
-function switch_eye(x) {
-  var name ="image_eye_"+x;
-  if (x=="1")
-  {
-    if (eye_status_1=="open")
-    {
-      document.getElementById(name).src = "/public/icons/login/no_eye.png";
-     // console.log(document.getElementById(name).src)
-      eye_status_1 = "close";
-    }
-    else if (eye_status_1=="close")
-    {
-      document.getElementById(name).src = "/public/icons/login/eye.png";
-     // console.log(document.getElementById(name).src)
-      eye_status_1 = "open";
-    }
-  }
-  else if (x=="2")
-  {
-    if (eye_status_2=="open")
-    {
-      document.getElementById(name).src = "/public/icons/login/no_eye.png";
-     // console.log(document.getElementById(name).src)
-      eye_status_2 = "close";
-    }
-    else if (eye_status_2=="close")
-    {
-      document.getElementById(name).src = "/public/icons/login/eye.png";
-     // console.log(document.getElementById(name).src)
-      eye_status_2 = "open";
-    }
-  }
-}
-
-
-function seePass(x){
-  var pwd = document.getElementById('password'+x);
-  if(pwd.type == 'password')
-  {
-    pwd.type = 'text'
-  }
-  else
-  {
-    pwd.type = 'password';
-  }
-}
-
-
-async function animate() {
-  this.classList.add('exit');
-  this.classList.remove('play');
-
-  await new Promise(r => setTimeout(r, 500));
-  this.classList.remove('exit');
-  this.classList.add('play');
-};
-
-
